9487194ba781bad57630e60d122ccae678957e1a
Фонтош Иван <fontosh.ivan@gmail.com>
2021-04-14 13:15:27 +0800

    - reg

--- server/config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  "MYSQL_HOST": "212.24.100.23",
-  "MYSQL_USER": "fontosh",
-  "MYSQL_SECRET" : "afirVhj2pR6avIpc",
-  "MYSQL_DB_NAME": "fontosh_folkbook",
-  "MYSQL_PORT": "3306"
-};
