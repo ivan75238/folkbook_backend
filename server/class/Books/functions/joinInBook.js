55647f3e0f97fc6c3ea61bbad1eccbf79cf12bef
Фонтош Иван <fontosh.ivan@gmail.com>
2021-04-23 13:26:13 +0800

    - старт книги, по ее заполнению

--- server/class/Books/functions/joinInBook.js
+++ server/class/Books/functions/joinInBook.js
@@ -1,6 +1,9 @@
 import MySQL from "../../mysql";
 import {HTTPStatus} from "../../HTTPStatus";
 import {checkParams} from "../../unitls";
+import moment from 'moment';
+
+const ADD_COUNT_DAY = 2;
 
 export const joinInBook = async (req, res) => {
     if (!checkParams(req, ["id_book", "id_user"])) {
@@ -16,6 +19,18 @@ export const joinInBook = async (req, res) => {
     result = await mysql.query(`SELECT * FROM \`participants_in_book\` WHERE \`id_book\` = '${req.body.id_book}';`);
     if (result[0].length < book.max_participants) {
         await mysql.query(`INSERT INTO \`participants_in_book\` (\`id_user\`, \`id_book\`) VALUES ('${req.body.id_user}', '${req.body.id_book}')`);
+        //Если участник последний, то передвигаем начало книги на ближайшее время
+        const started_at = moment(book.started_at);
+        //Если между датой старта и текущей датой меньше часа, то не трогаем ничего
+        //Иначе передвинем на ближйший час
+        if (moment().diff(started_at, 'hours') > 1) {
+            const new_started_at = moment().add(1, "h").set({minute: 0, second: 0}).format("YYYY-MM-DD HH:mm:ss");
+            await mysql.query(`UPDATE \`books\` SET \`started_at\` = '${new_started_at}' WHERE \`id\` = '${req.body.id_book}';`);
+            const chapter = (await mysql.query(`SELECT * FROM \`chapters\` WHERE \`id_book\` = '${req.body.id_book}'`))[0][0];
+            const section = (await mysql.query(`SELECT * FROM \`sections\` WHERE \`id_chapter\` = '${chapter.id}'`))[0][0];
+            const vote_finished_at = moment(new_started_at).add(ADD_COUNT_DAY, "days").set({second: 0}).format("YYYY-MM-DD HH:mm:ss");
+            await mysql.query(`UPDATE \`sections\` SET \`finished_at\` = '${new_started_at}', \`updated_at\` = NOW(), \`vote_finished_at\` = '${vote_finished_at}' WHERE \`id\` = '${section.id}';`);
+        }
         mysql.close();
         res.send({result: true});
     }
