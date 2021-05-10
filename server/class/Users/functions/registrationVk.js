0f5a86f06741102213afe9fcce93ac9c2194d36c
Фонтош Иван <fontosh.ivan@gmail.com>
2021-05-10 11:43:05 +0800

    - авториация вк

--- server/class/Users/functions/registrationVk.js
+++ server/class/Users/functions/registrationVk.js
@@ -3,8 +3,8 @@ import MySQL from "../../mysql";
 export const registrationVk = async (req, res) => {
     const mysql = new MySQL();
     await mysql.query(`
-        INSERT INTO \`users\` (\`username\`, \`password\`, \`nickname\`, \`is_active\`, \`provider\`) 
-        VALUES ('${req.body.username}', '${req.body.password}', '${req.body.first_name} ${req.body.last_name}', '1', 'vk');`
+        INSERT INTO \`users\` (\`username\`, \`password\`, \`nickname\`, \`is_active\`, \`provider\`, \`vk_link\`) 
+        VALUES ('${req.body.username}', '${req.body.password}', '${req.body.first_name} ${req.body.last_name}', '1', 'vk', '${req.body.href}');`
     );
     mysql.close();
     return res.send({result: true});
